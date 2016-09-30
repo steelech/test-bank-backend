8baac6efa4bfb6eb9db0858f28dc6fa4ee8d3fd3
Charlie Steele <steelech@umich.edu>
2016-09-29 23:54:15 -0400

    Initial commit

---
+++ app/controllers/sign_controller.rb
@@ -0,0 +1,48 @@
+class SignController < ApplicationController
+
+	def sign
+
+		@expires = 10.hours.from_now.utc.iso8601
+		render json: {
+			acl: 'public-read',
+			#awsaccesskeyid: @aws_access_key_id,
+			awsaccesskeyid: ENV['AWS_ACCESS_KEY_ID'],
+		        bucket: 'test-bank-assets',
+			expires: @expires,
+			key: "uploads/#{params[:name]}",
+			policy: policy,
+			signature: signature,
+			success_action_status: '201',
+			'Content-Type' => params[:type],
+			'Cache-Control' => 'max-age=630720000, public'
+		}, status: :ok
+	end
+
+	def signature
+		Base64.strict_encode64(
+			OpenSSL::HMAC.digest(
+				OpenSSL::Digest.new('sha1'),
+				ENV['AWS_SECRET_ACCESS_KEY'],
+				policy({secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']})
+			)
+		)
+	end
+	
+	def policy(options = {})
+		Base64.strict_encode64(
+			{
+				expiration: @expires,
+				conditions: [
+					{ bucket: 'test-bank-assets' },
+					{ acl: 'public-read' },
+					{ expires: @expires },
+					{ success_action_status: '201'},
+					[ 'starts-with', '$key', ''], 
+					[ 'starts-with', '$Content-Type', ''],
+					[ 'content-length-range', 0, 524288000]
+       
+				]
+			}.to_json
+		)
+	end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Backend::Application.config.secret_token = '1839862be760e76d487ecb202813a4177ebb1997d165534f0a44531ebd669ae6866f4d88b926f9b842a702603e65ffc9f1dafb3248f78fdd61bc3026a443711a'
