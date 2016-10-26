eb5e4e17c93651d667bd2392b433913c48991b6e
Charlie Steele <steelech@umich.edu>
2016-10-26 00:52:15 -0400

    Create document object on file upload

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -11,43 +11,52 @@ class UploadsController < AuthenticatedController
 		#folder = File.join(Rails.root, "tmp", "uploads")
 		#Dir.mkdir(folder) unless File.exists?(folder)
 		#pdf.save "#{Rails.root}/tmp/uploads/combined.pdf"
-		puts params
-		save_pdf(params['files'])
+		#save_pdf(params['files'])
 
 		# now we need to upload the new file to s3
-		s3 = Aws::S3::Client.new(
-			access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-			secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-		)
-		s3Object = Aws::S3::Object.new(
-		'test-bank-assets', 'combined', { client: s3 })
+		#s3 = Aws::S3::Client.new(
+		#	access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+		#	secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+		#)
+		#s3Object = Aws::S3::Object.new(
+		#'test-bank-assets', 'combined', { client: s3 })
 
-		s3Object.upload_file("#{Rails.root}/tmp/uploads/combined.pdf")
+		#s3Object.upload_file("#{Rails.root}/tmp/uploads/combined.pdf")
 		# todo: delete tmp/uploads folder after upload, refactor this 
-		signer = Aws::S3::Presigner.new
-		url = signer.presigned_url(:get_object, bucket: "test-bank-assets", key: 'combined', expires_in: 30)
-		puts url
+		#signer = Aws::S3::Presigner.new
+		#url = signer.presigned_url(:get_object, bucket: "test-bank-assets", key: 'combined', expires_in: 30)
+		#puts url
 
-		cogClient = Aws::CognitoIdentity::Client.new(
-			region: "us-west-2",
-			access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-			secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+		#cogClient = Aws::CognitoIdentity::Client.new(
+		#	region: "us-west-2",
+		#	access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+		#	secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 
-		)	
-		puts "CLIENT IS GOOD"
-		resp = cogClient.get_open_id_token_for_developer_identity({
-			identity_pool_id: 'us-west-2:956f2a12-44f6-4adb-906e-c0fc4c103649',
-			logins: {
-				"login.testbank" => "steelech@umich.edu"
-			},
-		})
-		puts "Response: #{resp.inspect}"
-		puts "identity_id: #{resp.identity_id}"
-		puts "token: #{resp.token}"
-		
+		#)	
+		#puts "CLIENT IS GOOD"
+		#resp = cogClient.get_open_id_token_for_developer_identity({
+		#	identity_pool_id: 'us-west-2:10100c61-ea97-4255-ae90-be2c1a63ade0',
+		#	logins: {
+		#		"login.testbank" => "steelech@umich.edu"
+		#	},
+		#})
+		files = params['files']
+		email = params['email']
+		puts "email: #{email}"
+		name = params['name']
+		course = params['course']
+		filename = params['files'][0].original_filename
+	
+		user = User.find_by(email: email)
+		puts "user: #{user.email}"
+		user.documents.create({name: name, course: course, s3_key: filename, s3_bucket: 'test-bank-assets'})
+
 		data = {
-			identity_id: resp.identity_id,
-			token: resp.token
+			name: name,
+			course: course,
+			s3_key: filename,
+			s3_bucket: 'test-bank-assets'
+
 		}
 		render json: data, status: 202
 
