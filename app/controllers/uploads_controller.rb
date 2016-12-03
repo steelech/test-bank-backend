493a01175aea431a16078c87f564af62f82a26ce
Charlie Steele <steelech@umich.edu>
2016-12-03 11:10:13 -0500

    Handle single and multi-file uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -12,72 +12,23 @@ class UploadsController < AuthenticatedController
 			course = Course.where(name: course_name).first
 			s3_key = params["data"]["attributes"]["s3-key"]
 		        @upload = Upload.create!({name: name, course: course, s3_key: s3_key})
-
 		else
-			puts "#{num_files} files!!!"
+			# TODO: handle multiple files, each with a name and course
+			name = params["name"]
+			course_name = params["course"]
+			bucket = "test-bank-assets"
+			s3_key = name.downcase.gsub(/\s/, "") + course_name.downcase.gsub(/\s/, "")
+			save_pdf(files)
+			s3 = Aws::S3::Client.new(
+				access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+				secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+			)
+			s3Object = Aws::S3::Object.new(bucket, s3_key, client: s3)
+			s3Object.upload_file("#{Rails.root}/tmp/uploads/combined.pdf")
+			course = Course.where(name: course_name).first
+			@upload = Upload.create({name: name, course: course, s3_key: s3_key})
+			render json: @upload, status: 202
 		end
-		count = 0
-		while count < num_files.to_i
-			file = params["file-#{count}"]
-			puts "file: #{file}"
-			count = count + 1
-		end
-			
-
-		# combine the pdfs into one file and store in /tmp
-		#puts "params: #{params}"
-		#pdf = CombinePDF.new
-		#params['files'].each do |file|
-		#	pdf << CombinePDF.new(file.tempfile.path)
-		#end
-		#folder = File.join(Rails.root, "tmp", "uploads")
-		#Dir.mkdir(folder) unless File.exists?(folder)
-		#pdf.save "#{Rails.root}/tmp/uploads/combined.pdf"
-		#save_pdf(params['files'])
-
-		# now we need to upload the new file to s3
-		#s3 = Aws::S3::Client.new(
-		#	access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-		#	secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-		#)
-		#s3Object = Aws::S3::Object.new(
-		#'test-bank-assets', 'combined', { client: s3 })
-
-		#s3Object.upload_file("#{Rails.root}/tmp/uploads/combined.pdf")
-		# todo: delete tmp/uploads folder after upload, refactor this 
-		#signer = Aws::S3::Presigner.new
-		#url = signer.presigned_url(:get_object, bucket: "test-bank-assets", key: 'combined', expires_in: 30)
-		#puts url
-
-		#cogClient = Aws::CognitoIdentity::Client.new(
-		#	region: "us-west-2",
-		#	access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-		#	secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-
-		#)	
-		#puts "CLIENT IS GOOD"
-		#resp = cogClient.get_open_id_token_for_developer_identity({
-		#	identity_pool_id: 'us-west-2:10100c61-ea97-4255-ae90-be2c1a63ade0',
-		#	logins: {
-		#		"login.testbank" => "steelech@umich.edu"
-		#	},
-		#})
-	
-		#user = User.find_by(email: email)
-		#puts "user: #{user.email}"
-		#user.uploads.create({name: name, course: course, s3_key: filename, s3_bucket: 'test-bank-assets'})
-
-		#data = {
-			#name: name,
-			#course: course,
-			#s3_key: filename,
-			#s3_bucket: 'test-bank-assets'
-
-		#}
-		render json: {}, status: 202
-
-
-
 	end
 
 	def index
@@ -85,6 +36,14 @@ class UploadsController < AuthenticatedController
 	end
 
 	private
+
+	def save_file_record(params)
+		Upload.create!({name: params.name})
+
+	end
+
+	def combine_files(params) 
+	end
 
 	def uploads_query(params)
 		if params[:course]
@@ -118,5 +77,19 @@ class UploadsController < AuthenticatedController
 		folder = File.join(Rails.root, "tmp", "uploads")
 		Dir.mkdir(folder) unless File.exists?(folder)
 		pdf.save "#{Rails.root}/tmp/uploads/combined.pdf"
+	end
+	
+	def files()
+		puts "params: #{params}"
+		num_files = params["files"].to_i
+		count = 0
+		@files = []
+		while count < num_files.to_i
+			file = params["file-#{count}"]
+			@files.push(file)
+			puts "file: #{file}"
+			count = count + 1
+		end
+		@files
 	end
 end
